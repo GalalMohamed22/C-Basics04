2ce80a9c0940848bf09dcd4ac929eb9a3997b1dc
GalalMohamed22 <galal9678@gmail.com>
2026-08-26 05:42:28 +0300

    Add project files.

---
+++ Assignment3/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Book
+    {
+        private string password = "Secret";
+        internal int copiesInStock = 5;
+        public string Title = "Clean Code";
+        public Genre genre = Genre.Science;
+
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why?
+
+            //Book book = new Book();
+            //Console.WriteLine(book.password);
+            // not show the variable passowrd because it private
+            // private means show only inside the class
+
+
+            #endregion
+
+           
+
+        }
+    }
+}
