7f6c0c56c28e3a3773c83dc65e4968a10c234033
GalalMohamed22 <galal9678@gmail.com>
2026-08-26 05:46:22 +0300

    Question02

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -6,7 +6,6 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-
             #region Add a private string password = "secret"; field to a Book class. Try to print it from Main (outside the class). What happens, and why?
 
             //Book book = new Book();
@@ -17,9 +16,17 @@ namespace Assignment3
 
             #endregion
 
+            #region Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile? Why ?
+
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock);
+            // the value of copiesInStock show in console because it internal
+            // internal means show in the same assemply
+
+
+            #endregion
+
            
-
-
 
         }
     }
